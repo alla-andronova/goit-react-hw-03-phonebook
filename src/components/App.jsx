f858a07b4d62102352f5295e053edc6ac7e344f3
alla-andronova <allaitcreative@gmail.com>
2021-07-10 21:56:05 +0300

    adds local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import Filter from './filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -52,10 +47,27 @@ class App extends Component {
     });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
 
     const normalizedFilter = filter.toLowerCase();
+
     const filteredContacts = normalizedFilter
       ? contacts.filter(contact =>
           contact.name.toLowerCase().includes(normalizedFilter),
